5de2d501cc7187fa51613784c2e0e7ddde49872a
PabloWolf96 <ikhena.owen47@gmail.com>
2021-02-01 21:06:20 +0100

    add item to list

---
+++ static/script.js
@@ -0,0 +1,33 @@
+let add = document.getElementById('addForm');
+add.addEventListener('submit', addToList);
+let content = document.getElementById('content');
+
+
+function addToList(e) {   
+    e.preventDefault();
+    let item = document.createElement('div');
+    item.className = 'item';
+    let ul = document.createElement('ul');
+    ul.className = "task-list";
+    
+    let li = document.createElement('li');
+    let inp = document.createElement('input');
+    inp.className = "check";
+    inp.type = "checkbox";
+    li.appendChild(inp);
+    let span = document.createElement('span');
+    span.className = "t";
+    let task = document.getElementById('text-box').value;
+    span.textContent = task;
+    li.appendChild(span);
+    let img = document.createElement('img');
+    img.className = "garbage";
+    img.src = `..\\garbage.svg`;
+    li.appendChild(img);
+    ul.appendChild(li)
+    item.appendChild(ul)
+    let hr = document.createElement('hr');
+    item.append(hr);
+    content.appendChild(item);
+    
+}
