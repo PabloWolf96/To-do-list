45b8017a9d5920fc5355948df0005ff84d44f2b9
PabloWolf96 <ikhena.owen47@gmail.com>
2021-02-05 02:03:29 +0100

    check and count

--- static/script.js
+++ static/script.js
@@ -1,7 +1,11 @@
+"use strict";
 let add = document.getElementById('addForm');
 add.addEventListener('submit', addToList);
 let content = document.getElementById('content');
 content.addEventListener('click', removeFromList);
+let taskCount = document.querySelector('.task-count');
+content.addEventListener('change', completed);
+
 
 function addToList(e) {   
     e.preventDefault();
@@ -17,8 +21,8 @@ function addToList(e) {
     li.appendChild(inp);
     let span = document.createElement('span');
     span.className = "t";
-    let task = document.getElementById('text-box').value;
-    span.textContent = task;
+    let task = document.getElementById('text-box');
+    span.textContent = task.value;
     li.appendChild(span);
     let img = document.createElement('img');
     img.className = "garbage";
@@ -29,12 +33,51 @@ function addToList(e) {
     let hr = document.createElement('hr');
     item.append(hr);
     content.appendChild(item);
-    
+    task.value = "";
+    changeTaskCount("add");
 }
 function removeFromList(e) {
     if (e.target.className == "garbage"){
 
         let div = e.target.parentElement.parentElement.parentElement;
         content.removeChild(div);
+        changeTaskCount('delete');
+    }
+}
+function changeTaskCount(change) {
+    
+    let pattern = taskCount.textContent.match(/\d+/);
+    if (change == "delete") {
+        pattern = Number(pattern[0]);
+        if (pattern == 1) {
+            taskCount.textContent = `No active task`;
+        } else if (pattern == 2) {
+            taskCount.textContent = `${pattern - 1} active task`;
+        } else if (pattern > 2) {
+            taskCount.textContent = `${pattern - 1} active tasks`;
+        }
+        
+    }
+    if (change == "add") {
+        if (pattern === null) {
+            taskCount.textContent = `1 active task`;
+        } else {
+            pattern = Number(pattern[0]);
+            taskCount.textContent = `${pattern + 1} active tasks`;
+        }
+    }
+       
+
+}
+function completed(e) {
+    if (e.target.className == "check") {
+        let li = e.target.parentElement;
+        li.style.textDecoration = "line-through 3px white";
+        if (!e.target.checked) {
+            li.style.textDecoration = "none";
+
+        }
+
+
     }
 }
